6e36fb80d1d5e102653f74cb0aee789e88a9273b
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-15 18:29:25 -0400

    implemented a basic interpreter

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,28 @@
 module Main where
     
+import System.Environment
+import System.IO
 import Parser.Parser
 import Parser.Grammar
+import Interpreter.Interpreter
+import Interpreter.Stack
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = interpret emptyStack
+    
+-- | Run a persistent interpreter
+interpret :: Stack -> IO ()
+interpret stack = do
+    statement <- prompt
+    let newStack = case evalLine statement stack of
+                    Left err -> error $ err
+                    Right s -> s
+    putStrLn $ show newStack
+    interpret newStack
+    
+-- | Main prompt for the interactive interpreter 
+prompt :: IO String
+prompt = do
+    putStr $ ">>> "
+    hFlush stdout
+    getLine
